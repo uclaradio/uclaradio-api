0889b2594b34340d19de5205ae7ce97a6383daf8
Nathan Smith <nathan@nathansmith.io>
2018-02-09 19:13:52 -0800

    Add a test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
-import { Router, Request, Response, NextFunction } from 'express';
+import { Router, Request, Response, NextFunction } from "express";
 
 const router = Router();
 
 /** Main Pages */
-router.get('/', (req: Request, res: Response) => {
-  res.json('Hello World');
+router.get("/", (req: Request, res: Response) => {
+  res.json("Hello World");
 });
 
 export default router;
