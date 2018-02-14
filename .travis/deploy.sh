57e167875eb3e916a46a08f7009a33268cb3be52
Nathan Smith <nathan@nathansmith.io>
2018-02-14 11:02:56 -0800

    Add travis support! (#11)
    * Add a test
    
    * Add travis
    
    * add deploy key
    
    * change key
    
    * :uclaradio-api
    
    * Change key again
    
    * Adjust node settings
    
    * Update things
    
    * move types
    
    * Update
    
    * no need for build
    
    * Add badges (#14)
    
    * Add public key
    
    * Tanzeela show model (#7)
    
    * faker generates fake shows
    
    * faker data and attributes
    
    * slightly cleaned
    
    * Created SocialLink model (#8)
    
    * Started/finished? SL mode;
    
    * Finished and tested SocialLink
    
    * Removed package-lock
    
    * yarn
    
    * Update key
    
    * Add ts-jest
    
    * deploy script

---
+++ .travis/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy.key
+ssh-add .travis/deploy.key
+ssh-keyscan 192.241.200.33 >> ~/.ssh/known_hosts
+
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+  git remote add dokku dokku@192.241.200.33:uclaradio-api
+elif [[ "$TRAVIS_BRANCH" == "staging" ]]; then
+  git remote add dokku dokku@192.241.200.33:uclaradio-api-staging
+fi
+
+git config --global push.default simple
+
+if [[ $TRAVIS_BRANCH == "master" ]]; then
+  git push dokku master;
+elif [[ $TRAVIS_BRANCH == "staging" ]]; then
+  git push dokku staging:master
+fi
