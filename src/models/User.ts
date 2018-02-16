fc9466e58c601ee4f3c2a45c1b3edb7961861bc8
kianadk <kianadkavoosi@gmail.com>
2018-02-15 17:28:06 -0800

    create user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,16 @@ export interface UserInstance {}
 
 const User: Sequelize.Model<UserInstance, UserAttributes> = sequelize.define(
   'User',
-  {}
+  {
+    bio: Sequelize.STRING,
+    djName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    fullName: Sequelize.STRING,
+    password: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    picture: Sequelize.STRING,
+    shows: Sequelize.ARRAY(Sequelize.TEXT)
+  }
 );
 
 sequelize.sync();
