1c5f2afaa2f118abb3cdcdfa473caa1fabfc3caf
tanzeelak <tnkhan8042@gmail.com>
2018-03-02 13:30:59 -0800

    showController is more accurate

--- src/controllers/showController.test.ts
+++ src/controllers/showController.test.ts
@@ -2,37 +2,36 @@ import * as faker from 'faker';
 
 import { sequelize, Show } from '../models';
 import * as showController from './showController';
-import { ShowInstance } from '../models/Show';
 
 beforeEach(async () => {
-  await sequelize.sync({ force: true });
+    await sequelize.sync({ force: true });
 });
 
 function getRandomShow() {
-  return {
-    name: faker.name.findName(),
-    organization: faker.company.catchPhrase(),
-    phones: faker.phone.phoneNumberFormat(),
-    emails: faker.internet.email(),
-    notes: faker.lorem.sentence(),
-  };
+    return {
+        name: faker.commerce.productName(),
+        userIds: randoms,
+        description: faker.hacker.phrase(),
+        genre: faker.commerce.productAdjective(),
+        image: faker.image.avatar(),
+    };
 }
 
 describe('createShow', () => {
-  it('should add a show entry to the database', async () => {
-    const show = getRandomShow();
+    it('should add a show entry to the database', async () => {
+        const show = getRandomShow();
 
-    await showController.createShow(show);
+        await showController.createShow(show);
 
-    const showResultsArray = await Show.findAll({
-      where: {
-        name: show.name,
-      },
-    });
-    // We need the `plain: true` option so that we only get the object values we care about.
-    // See http://docs.sequelizejs.com/manual/tutorial/instances.html#values-of-an-instance
-    const showResult = showResultsArray[0].get({
-      plain: true,
+        const showResultsArray = await Show.findAll({
+            where: {
+                name: show.name,
+            },
+        });
+        // We need the `plain: true` option so that we only get the object values we care about.
+        // See http://docs.sequelizejs.com/manual/tutorial/instances.html#values-of-an-instance
+        const showResult = showResultsArray[0].get({
+        plain: true,
     });
 
     expect(showResultsArray).toHaveLength(1);
@@ -40,93 +39,93 @@ describe('createShow', () => {
     expect(showResult).toHaveProperty('id');
     expect(showResult).toHaveProperty('createdAt');
     expect(showResult).toHaveProperty('updatedAt');
-  });
+});
 });
 
 describe('getShow', () => {
-  it('retrieves a show from the database', async () => {
-    const show = {
-      name: faker.name.findName(),
-      organization: faker.company.catchPhrase(),
-      phones: faker.phone.phoneNumberFormat(),
-      emails: faker.internet.email(),
-      notes: faker.lorem.sentence(),
-    };
-    await Show.create(show);
+    it('retrieves a show from the database', async () => {
+        const show = {
+            name: faker.commerce.productName(),
+            userIds: randoms,
+            description: faker.hacker.phrase(),
+            genre: faker.commerce.productAdjective(),
+            image: faker.image.avatar()
+        };
+        await Show.create(show);
 
-    const retrievedShowInstance = await showController.getShow(1);
-    const retrievedShowAttributes = retrievedShowInstance.get({
-      plain: true,
+        const retrievedShowInstance = await showController.getShow(1);
+        const retrievedShowAttributes = retrievedShowInstance.get({
+            plain: true,
+        });
+
+        expect(retrievedShowAttributes).toMatchObject(show);
+        expect(retrievedShowAttributes).toHaveProperty('id');
+        expect(retrievedShowAttributes).toHaveProperty('createdAt');
+        expect(retrievedShowAttributes).toHaveProperty('updatedAt');
     });
 
-    expect(retrievedShowAttributes).toMatchObject(show);
-    expect(retrievedShowAttributes).toHaveProperty('id');
-    expect(retrievedShowAttributes).toHaveProperty('createdAt');
-    expect(retrievedShowAttributes).toHaveProperty('updatedAt');
-  });
-
-  it('returns null if a show with the specified ID cannot be found', async () => {
-    const retrievedShowInstance = await showController.getShow(2);
-    expect(retrievedShowInstance).toBeNull();
-  });
+    it('returns null if a show with the specified ID cannot be found', async () => {
+        const retrievedShowInstance = await showController.getShow(2);
+        expect(retrievedShowInstance).toBeNull();
+    });
 });
 
 describe('getAllShows', () => {
-  it('returns an empty array when the database is empty', async () => {
-    expect(await showController.getAllShows()).toHaveLength(0);
-  });
+    it('returns an empty array when the database is empty', async () => {
+        expect(await showController.getAllShows()).toHaveLength(0);
+    });
 
-  it('returns an array of all shows', async () => {
-    const show = getRandomShow();
-    await Show.create(show);
+    it('returns an array of all shows', async () => {
+        const show = getRandomShow();
+        await Show.create(show);
 
-    const allShows = await showController.getAllShows();
-    const retrievedShowAttributes = allShows[0].get({ plain: true });
+        const allShows = await showController.getAllShows();
+        const retrievedShowAttributes = allShows[0].get({ plain: true });
 
-    expect(allShows).toHaveLength(1);
-    expect(retrievedShowAttributes).toMatchObject(show);
-  });
+        expect(allShows).toHaveLength(1);
+        expect(retrievedShowAttributes).toMatchObject(show);
+    });
 });
 
 describe('updateShow', () => {
-  it('updates an existing show from the database', async () => {
-    await Show.create(getRandomShow());
+    it('updates an existing show from the database', async () => {
+        await Show.create(getRandomShow());
 
-    const newName = faker.name.findName();
+        const newName = faker.name.findName();
 
-    const updatedShowInstance = await showController.updateShow(1, {
-      name: newName,
-    });
-    const updatedShowAttributes = updatedShowInstance.get({
-      plain: true,
-    });
+        const updatedShowInstance = await showController.updateShow(1, {
+            name: newName,
+        });
+        const updatedShowAttributes = updatedShowInstance.get({
+            plain: true,
+        });
 
-    expect(updatedShowAttributes).toHaveProperty('name', newName);
-  });
-
-  it('returns null if a show with the specified ID cannot be found', async () => {
-    const updatedShowInstance = await showController.updateShow(2, {
-      name: faker.name.findName(),
+        expect(updatedShowAttributes).toHaveProperty('name', newName);
     });
 
-    expect(updatedShowInstance).toBeNull();
-  });
+    it('returns null if a show with the specified ID cannot be found', async () => {
+        const updatedShowInstance = await showController.updateShow(2, {
+            name: faker.name.findName(),
+        });
+
+        expect(updatedShowInstance).toBeNull();
+    });
 });
 
 describe('deleteShow', () => {
-  it('deletes a show from the database and returns true', async () => {
-    await Show.create(getRandomShow());
+    it('deletes a show from the database and returns true', async () => {
+        await Show.create(getRandomShow());
 
-    expect(await Show.findAll()).toHaveLength(1);
-    expect(await showController.deleteShow(1)).toBe(true);
-    expect(await Show.findAll()).toHaveLength(0);
-  });
+        expect(await Show.findAll()).toHaveLength(1);
+        expect(await showController.deleteShow(1)).toBe(true);
+        expect(await Show.findAll()).toHaveLength(0);
+    });
 
-  it('does not delete a show from the database and returns false when the id is invalid', async () => {
-    await Show.create(getRandomShow());
+    it('does not delete a show from the database and returns false when the id is invalid', async () => {
+        await Show.create(getRandomShow());
 
-    expect(await Show.findAll()).toHaveLength(1);
-    expect(await showController.deleteShow(2)).toBe(false);
-    expect(await Show.findAll()).toHaveLength(1);
-  });
+        expect(await Show.findAll()).toHaveLength(1);
+        expect(await showController.deleteShow(2)).toBe(false);
+        expect(await Show.findAll()).toHaveLength(1);
+    });
 });
--- src/controllers/showController.ts
+++ src/controllers/showController.ts
@@ -2,74 +2,74 @@ import { Show } from '../models';
 import { ShowAttributes, ShowInstance} from '../models/Show';
 
 /**
- * Creates a show with the given attribues
- *
- * @param {ShowAttributes} attributes
- * @returns The newly created show.
- */
+* Creates a show with the given attribues
+*
+* @param {ShowAttributes} attributes
+* @returns The newly created show.
+*/
 
 export async function createShow(
-  attributes: ShowAttributes
+    attributes: ShowAttributes
 ): Promise<ShowInstance> {
-  const newShow: ShowInstance = await Show.create(attributes);
-  return newShow;
+    const newShow: ShowInstance = await Show.create(attributes);
+    return newShow;
 }
 /**
- * Gets the show with the given id.
- *
- * @param {numberx} id
- */
+* Gets the show with the given id.
+*
+* @param {numberx} id
+*/
 export async function getShow(id: number): Promise<ShowInstance | null> {
-  const show: ShowInstance | null = await Show.findById(id);
-  return show;
+    const show: ShowInstance | null = await Show.findById(id);
+    return show;
 }
 
 /**
- * Gets all shows.
- *
- * @param id
- */
+* Gets all shows.
+*
+* @param {numberx} id
+*/
 export async function getAllShows(): Promise<ShowInstance[]> {
-  const shows: ShowInstance[] = await Show.all();
-  return shows;
+    const shows: ShowInstance[] = await Show.all();
+    return shows;
 }
 
 /**
- * Updates a show with the given attributes. Returns the updated show, or undefined if the show is not updated.
- *
- * @export
- * @param {number} id
- * @param {*} attributes
- * @returns {(any | undefined)}
- */
+* Updates a show with the given attributes. Returns the updated show, or undefined if the show is not updated.
+*
+* @export
+* @param {number} id
+* @param {*} attributes
+* @returns {(any | undefined)}
+*/
 export async function updateShow(
-  id: number,
-  attributes: ShowAttributes
+    id: number,
+    attributes: ShowAttributes
 ): Promise<ShowInstance | null> {
-  const [numberOfUpdatedShows, updatedShows] = await Show.update(
-    attributes,
-    {
-      where: { id },
-      returning: true,
-    }
-  );
-  if (numberOfUpdatedShows === 1) {
-    return updatedShows[0];
-} else if (numberOfUpdatedShows !== 0) {
-    // throw new
-    throw new Error(
-    'More than 1 rows updated from single id in `updateShow`! This is bad!'
-);
-  }
-  return null;
+    const [numberOfUpdatedShows, updatedShows] = await Show.update(
+        attributes,
+        {
+            where: { id },
+            returning: true,
+        }
+    );
+    if (numberOfUpdatedShows === 1) {
+        return updatedShows[0];
+    } else if (numberOfUpdatedShows !== 0) {
+        // throw new
+        throw new Error(
+        'More than 1 rows updated from single id in `updateShow`! This is bad!'
+    );
+}
+return null;
 
 }
 
 /**
- * Deletes a show. Returns true if the show was successfully deleted, false otherwise.
- *
- * @param id
- */
+* Deletes a show. Returns true if the show was successfully deleted, false otherwise.
+*
+* @param id
+*/
 export async function deleteShow(id: number): Promise<boolean> {
     const numberOfDeletedShows = await Show.destroy({
         where: { id },
@@ -80,5 +80,5 @@ export async function deleteShow(id: number): Promise<boolean> {
     } else if (numberOfDeletedShows !== 0 ){
         throw new Error('More than 1 row deleted from single id! This is bad!');
     }
-  return false;
+    return false;
 }
