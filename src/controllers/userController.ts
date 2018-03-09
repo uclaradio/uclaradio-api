0746e8ad2a6bd92520f1a54ad71e224e7769da13
Alex X Xu <xualexander1@gmail.com>
2018-03-08 18:16:26 -0800

    getDj and getAllDJs

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,16 +1,24 @@
-import { UserAttributes } from '../models/User';
+import  { User } from '../models';
+
+import { UserAttributes, UserInstance } from '../models/User';
 
 /**
  * Gets a DJ with the specified id.
  *
- * @param id
+ * @param {numberx} id
  * @returns {} DJ
  */
-export function getDJ(id: number) {}
+export async function getDJ(id: number) : Promise<ShowInstance | null> {
+  const dj: ShowInstance | null = await User.findById(id);
+  return dj;
+}
 
 /**
  * Gets all current DJs.
  *
  * @returns DJs
  */
-export function getAllDJs() {}
+export function getAllDJs() : Promise<ShowInstance[]> {
+  const djs : ShowInstance[] = await User.all();
+  return djs;
+}
